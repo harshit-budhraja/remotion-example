9926433b0b3898e0c9cc69cf46362f7a8cbf0070
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2025-01-15 15:47:47 +0530

    feat: working composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,12 +2,41 @@ import './tailwind.css';
 import { Composition } from "remotion";
 import { MyComposition } from "./Composition";
 
+const defaultProps = {
+  unfake: {
+    name: "Unfake",
+    repo: "unfake.dev",
+    logo: "https://ik.imagekit.io/oq1pyotaq/unfake/unfake-logo-shield.png",
+  },
+  react: {
+    name: "React",
+    repo: "facebook/react",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+  },
+  imagekit: {
+    name: "ImageKit",
+    repo: "imagekit.io",
+    logo: "https://pbs.twimg.com/profile_images/1394248978456190986/VdHMA6Oh_400x400.jpg",
+  },
+  typescript: {
+    name: "TypeScript",
+    repo: "microsoft/TypeScript",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg",
+  },
+  javascript: {
+    name: "JavaScript",
+    repo: "javascript/javascript",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
+  },
+}
+
 export const RemotionRoot: React.FC = () => {
   return (
     <>
       <Composition
         id="MyComp"
         component={MyComposition}
+        defaultProps={defaultProps.react}
         durationInFrames={60}
         fps={30}
         width={1280}
